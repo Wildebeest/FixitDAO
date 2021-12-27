7e4c1f27ca5f3dae009996c6688785ac891053e2
Matt Wilde <matthewcwilde@gmail.com>
2021-12-27 03:46:07 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {useEffect, useMemo, useState} from "react";
+import {useWeb3} from "@3rdweb/hooks";
+
+const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("Address: ", address);
+
+  if(!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to FixitDAO</h1>
+        <button onClick={() => connectWallet("injected")}   className="btn-hero">
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="landing">
+      <h1>Wallet connected!</h1>
+    </div>
+  );
+};
+
+export default App;
